d355c635ac5384735cb7132ceea1cf4db8d41ef1
ChrisClark13 <krizasan@gmail.com>
2021-05-18 17:02:01 -0600

    adding mithril routing for threads AND posts! fixed BBImages! nearly done

--- src/archiveResolver.js
+++ src/archiveResolver.js
@@ -11,7 +11,12 @@ function prependPath(path) {
 }
 
 function getUserAvatarLocationByID(id) {
-    return prependPath(imageIndex.userAvatarIndex[id]) || '.\\imageMissing.png';
+    let path = imageIndex.userAvatarIndex[id];
+    if (path) {
+        return prependPath(path)
+    } else {
+        return ".\\imageMissing.png";
+    }
 }
 
 function getUserAvatarLocationByName(name) {
@@ -26,17 +31,30 @@ function getUserAvatarLocationByName(name) {
     return '.\\imageMissing.png';
 }
 
+function getBBImageLocationByHash(hash) {
+    let path = imageIndex.bbImageIndex[hash];
+    if (path) {
+        return prependPath(path)
+    } else {
+        return ".\\imageMissing.png";
+    }
+}
+
+function getThreadPath(threadID, pageNum) {
+    return prependPath(`${threadIndex[threadID].path}\\page-${pageNum}.html`);
+}
+
 function $getXHRThreadPageByID(threadID, pageNum) {
-    return $.get(prependPath(`${threadIndex[threadID].path}\\page-${pageNum}.html`))
+    return $.get(getThreadPath(threadID, pageNum))
         .then(data => {
-            //console.log($('<div></div>').append($.parseHTML(data)));
-            let $body = $('<div></div>').append($.parseHTML(data)).find('.p-body-inner');
+            let $body = $($.parseHTML(data)).find('.p-body');
             
             //It's time for clean up!
             //Remove crap that's cluttering or just doesn't work.
+            $body.find('noscript').remove();
             $body.find('.notices').remove();
             $body.find('.shareButtons').remove();
-            $body.find('i.fal').remove();
+            $body.find('i[class^=fa]').remove();
             $body.find('li').has('a[data-xf-init="share-tooltip"').remove();
             $body.find('div.block-outer-opposite').remove();
             $body.find('img[src^="\/styles"]').remove();
@@ -50,8 +68,12 @@ function $getXHRThreadPageByID(threadID, pageNum) {
                     //console.log(href);
                     //Keep these ones
                     //TODO: Do some fancy routing to fix these too
-                    if (href.match(/\?threads\/.+?\.(?<threadID>\d+)\/|post-(?<postID>\d+)$/)) {
-
+                    let match;
+                    if (href.match(/\?threads\/.+?\.\d+\/|post-\d+$/)) {
+                        $a.attr('href', href.replace('index.php?', "#!/"));
+                    } else if ((match = href.match(/post&id=(\d+)/)) != null) {
+                        //console.log(match);
+                        $a.attr('href', `#!/post/${match[1]}`);
                     } else {
                         $a.removeAttr('href');
                     }
@@ -65,17 +87,56 @@ function $getXHRThreadPageByID(threadID, pageNum) {
                 $img.attr('src', getUserAvatarLocationByID(userID));
             });
 
+            //Fix images
+            let $images = $body.find('img.bbImage');
+            if ($images.length > 0) {
+                $images.each((i, e) => {
+                    $img = $(e);
+                    let srcUrl = $img.attr('src');
+                    
+                    if (srcUrl == null) {
+                        return;
+                    }
+
+                    //Bad url filtering
+                    if (srcUrl.match(/file:\/\/\/C/)) {
+                        return;
+                    }
+                    
+                    //console.log(srcUrl);
+                    let hashCaptures = srcUrl.match(/hash=(?<hash1>.+)$|attachments\/\d+\/(?<hash2>[^\.]+)\.|attachments\/(?<hash3>[^\/]+)/);
+                    if (hashCaptures == null) {
+                        console.log(`Unknown url format, cannot extract hash: ${srcUrl}`);
+                        $img.attr('.\\imageMissing.png');
+                    }
+                    
+                    let hash;
+                    if (hashCaptures.groups.hash1 !== undefined) {
+                        hash = hashCaptures.groups.hash1;
+                    } else if (hashCaptures.groups.hash2 !== undefined) {
+                        hash = hashCaptures.groups.hash2;
+                    } else if (hashCaptures.groups.hash3 !== undefined) {
+                        hash = hashCaptures.groups.hash3;
+                    }
+
+                    $img.attr('src', getBBImageLocationByHash(hash));
+                });
+            }
+
             return $body;
         });
 }
 
-function getPostByID(postID) {
-
+function getPostInfoByID(postID) {
+    return postIndex[postID];
 }
 
 module.exports = {
     prependPath,
     getUserAvatarLocationByID,
     getUserAvatarLocationByName,
-    $getXHRThreadPageByID
+    getBBImageLocationByHash,
+    getThreadPath,
+    $getXHRThreadPageByID,
+    getPostInfoByID
 }
--- src/index.js
+++ src/index.js
@@ -2,15 +2,69 @@ const m = require("mithril")
 const archive = require('./archiveResolver.js');
 const $ = require("jquery");
 
-m.render(document.body,
-    m('div', [
-        m('img', {src: archive.getUserAvatarLocationByName("Liltwick")})
-    ])
-);
+let $threadBody = null;
+let postID = null;
+let errorMessage = "";
 
-archive.$getXHRThreadPageByID(281788, 1).then( $body => {
-    console.log($body);
-    m.render(document.body,
-        m.trust($body.html())
-    );
-});
+const ThreadResolver = {
+    onmatch: function(args) {
+        return new Promise((resolve, reject) => {
+            let threadID = 0;
+            let pageNum = 1;
+
+            $threadBody = null;
+            postID = null;
+
+            if (args.threadID) {
+                threadID = args.threadID.match(/\d+$/)[0];
+            } else if (args.postID) {
+                postID = args.postID.match(/\d+$/)[0];
+                [threadID, pageNum] = archive.getPostInfoByID(postID);
+            } else {
+                errorMessage = "Invalid URL";
+            }
+            
+            if (args.pageNum) {
+                if (args.pageNum.includes('post')) {
+                    postID = args.pageNum.match(/\d+$/)[0]
+                    pageNum = archive.getPostInfoByID(postID)[1];
+                } else {
+                    pageNum = args.pageNum ? args.pageNum.match(/\d+$/)[0] : 1;
+                }
+            }
+
+            console.log(`Loading ${threadID}\\${pageNum}` + (postID == null ? '' : ` Pointing at postID ${postID}`));
+
+            archive.$getXHRThreadPageByID(threadID, pageNum)
+                .then( $body => {
+                    $threadBody = $body;
+                    resolve();
+                }).catch(err => {
+                    errorMessage = err;
+                    console.log(err);
+                }
+            )
+        }).catch(err => {
+            errorMessage = err;
+            console.log(err);
+        });
+    },
+    render: function(vnode) {
+        //console.log(vnode);
+        let content = ($threadBody != null) ? m.trust($threadBody.html()) : m('div', `Something went wrong, check the console...\nERROR\n${errorMessage}`);
+        return (postID == null) ? 
+            m('div', content) :
+            m('div', {
+                oncreate: () => document.getElementById(`post-${postID}`).scrollIntoView(true),
+                onupdate: () => document.getElementById(`post-${postID}`).scrollIntoView(true) 
+            }, content);
+    }
+}
+
+m.route(document.body, "/threads/281788/1",
+    {
+        "/post/:postID": ThreadResolver,
+        "/threads/:threadID": ThreadResolver,
+        "/threads/:threadID/:pageNum": ThreadResolver
+    }
+);
