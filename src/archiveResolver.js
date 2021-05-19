df44bba848a3245d3e62d7a43a27ce320d722340
ChrisClark13 <krizasan@gmail.com>
2021-05-18 19:05:33 -0600

    Added subforum directories, added missing css pieces

--- src/archiveResolver.js
+++ src/archiveResolver.js
@@ -1,6 +1,7 @@
 const sanitize = require('sanitize-filename');
 const $ = require('jquery');
 
+const subforumIndex = require(`./indexes/subforumIndex.json`);
 const threadIndex = require('./indexes/threadIndex.json');
 const postIndex = require('./indexes/postIndex.json');
 const imageIndex = require('./indexes/imageIndex.json');
@@ -41,91 +42,117 @@ function getBBImageLocationByHash(hash) {
 }
 
 function getThreadPath(threadID, pageNum) {
-    return prependPath(`${threadIndex[threadID].path}\\page-${pageNum}.html`);
+    let entry = threadIndex[threadID];
+    if (entry) {
+        return prependPath(`${entry.path}\\page-${pageNum}.html`);
+    } else {
+        throw `No such thread for threadID ${threadID}`; 
+    }
+}
+
+function getSubforumPath(subforumID, pageNum) {
+    let entry = subforumIndex[subforumID];
+    if (entry) {
+        return prependPath(`${entry.path}\\page-${pageNum}.html`);
+    } else {
+        throw `No such subforum for subforumID ${subforumID}`;
+    }
 }
 
 function $getXHRThreadPageByID(threadID, pageNum) {
-    return $.get(getThreadPath(threadID, pageNum))
-        .then(data => {
-            let $body = $($.parseHTML(data)).find('.p-body');
+    return $.get(getThreadPath(threadID, pageNum)).then(processHTML);
+}
+
+function $getXHRSubforumByID(subforumID, pageNum) {
+    return $.get(getSubforumPath(subforumID, pageNum)).then(processHTML);
+}
+
+function processHTML(data) {
+    let $body = $($.parseHTML(data)).find('.p-body');
             
-            //It's time for clean up!
-            //Remove crap that's cluttering or just doesn't work.
-            $body.find('noscript').remove();
-            $body.find('.notices').remove();
-            $body.find('.shareButtons').remove();
-            $body.find('i[class^=fa]').remove();
-            $body.find('li').has('a[data-xf-init="share-tooltip"').remove();
-            $body.find('div.block-outer-opposite').remove();
-            $body.find('img[src^="\/styles"]').remove();
-            $body.find('form.js-quickReply').remove();
+    //It's time for clean up!
+    //Remove crap that's cluttering or just doesn't work.
+    $body.find('noscript').remove();
+    $body.find('.notices').remove();
+    $body.find('.shareButtons').remove();
+    $body.find('i[class^=fa]').remove();
+    $body.find('li').has('a[data-xf-init="share-tooltip"').remove();
+    $body.find('div.block-outer-opposite').remove();
+    $body.find('img[src^="\/styles"]').remove();
+    $body.find('form.js-quickReply').remove();
 
-            //Fix links
-            $body.find('a').each((i, e) => {
-                let $a = $(e);
-                let href = $a.attr('href');
-                //We only care about internal links
-                if (href && (href.match(/^\/index\.php\??|^https:\/\/forums\.bulba/))) {
-                    //console.log(href);
-                    //Keep these ones
-                    //TODO: Do some fancy routing to fix these too
-                    let match;
-                    if (href.match(/\?threads\/.+?\.\d+\/|post-\d+$/)) {
-                        $a.attr('href', href.replace(/.+?index.php\?/, "#!/"));
-                    } else if ((match = href.match(/post&id=(\d+)/)) != null) {
-                        //console.log(match);
-                        $a.attr('href', `#!/post/${match[1]}`);
-                    } else {
-                        $a.removeAttr('href');
-                    }
+    //Fix links
+    $body.find('a').each((i, e) => {
+        let $a = $(e);
+        let href = $a.attr('href');
+        //We only care about internal links
+        if (href && (href.match(/^\/index\.php\??|^https:\/\/forums\.bulba/))) {
+
+            //Keep these ones
+            let match;
+            if (href.match(/\?threads\/.+?\.\d+\/|post-\d+$/)) {
+                $a.attr('href', href.replace(/.+?index.php\?/, "#!/"));
+            } else if ((match = href.match(/post&id=(\d+)/)) != null) {
+                //console.log(match);
+                $a.attr('href', `#!/post/${match[1]}`);
+            } else if ((match = href.match(/\?forums\/.+?\.(\d+)\//)) != null) {
+                //Save forum links... but only to the ones that we actually downloaded (didn't download those that had no threads!)
+                if (subforumIndex[match[1]]) {
+                    $a.attr('href', href.replace(/.+?index.php\?/, "#!/"));
+                } else {
+                    $a.removeAttr('href');
                 }
-            })
+            } else {
+                //Yeet these ones out
+                $a.removeAttr('href');
+            }
+        }
+    })
 
-            //Fix user avatars
-            $body.find('img[class^="avatar"').each((i, e) => {
-                $img = $(e);
-                let userID = $img.parent().attr('data-user-id');
-                $img.attr('src', getUserAvatarLocationByID(userID));
-            });
+    //Fix user avatars
+    $body.find('img[class^="avatar"').each((i, e) => {
+        $img = $(e);
+        let userID = $img.parent().attr('data-user-id');
+        $img.attr('src', getUserAvatarLocationByID(userID));
+    });
 
-            //Fix images
-            let $images = $body.find('img.bbImage');
-            if ($images.length > 0) {
-                $images.each((i, e) => {
-                    $img = $(e);
-                    let srcUrl = $img.attr('src');
-                    
-                    if (srcUrl == null) {
-                        return;
-                    }
-
-                    //Bad url filtering
-                    if (srcUrl.match(/file:\/\/\/C/)) {
-                        return;
-                    }
-                    
-                    //console.log(srcUrl);
-                    let hashCaptures = srcUrl.match(/hash=(?<hash1>.+)$|attachments\/\d+\/(?<hash2>[^\.]+)\.|attachments\/(?<hash3>[^\/]+)/);
-                    if (hashCaptures == null) {
-                        console.log(`Unknown url format, cannot extract hash: ${srcUrl}`);
-                        $img.attr('.\\imageMissing.png');
-                    }
-                    
-                    let hash;
-                    if (hashCaptures.groups.hash1 !== undefined) {
-                        hash = hashCaptures.groups.hash1;
-                    } else if (hashCaptures.groups.hash2 !== undefined) {
-                        hash = hashCaptures.groups.hash2;
-                    } else if (hashCaptures.groups.hash3 !== undefined) {
-                        hash = hashCaptures.groups.hash3;
-                    }
-
-                    $img.attr('src', getBBImageLocationByHash(hash));
-                });
+    //Fix images
+    let $images = $body.find('img.bbImage');
+    if ($images.length > 0) {
+        $images.each((i, e) => {
+            $img = $(e);
+            let srcUrl = $img.attr('src');
+            
+            if (srcUrl == null) {
+                return;
             }
 
-            return $body;
+            //Bad url filtering
+            if (srcUrl.match(/file:\/\/\/C/)) {
+                return;
+            }
+            
+            //console.log(srcUrl);
+            let hashCaptures = srcUrl.match(/hash=(?<hash1>.+)$|attachments\/\d+\/(?<hash2>[^\.]+)\.|attachments\/(?<hash3>[^\/]+)/);
+            if (hashCaptures == null) {
+                console.log(`Unknown url format, cannot extract hash: ${srcUrl}`);
+                $img.attr('.\\imageMissing.png');
+            }
+            
+            let hash;
+            if (hashCaptures.groups.hash1 !== undefined) {
+                hash = hashCaptures.groups.hash1;
+            } else if (hashCaptures.groups.hash2 !== undefined) {
+                hash = hashCaptures.groups.hash2;
+            } else if (hashCaptures.groups.hash3 !== undefined) {
+                hash = hashCaptures.groups.hash3;
+            }
+
+            $img.attr('src', getBBImageLocationByHash(hash));
         });
+    }
+
+    return $body;
 }
 
 function getPostInfoByID(postID) {
@@ -139,5 +166,6 @@ module.exports = {
     getBBImageLocationByHash,
     getThreadPath,
     $getXHRThreadPageByID,
+    $getXHRSubforumByID,
     getPostInfoByID
 }
--- src/index.js
+++ src/index.js
@@ -2,16 +2,18 @@ const m = require("mithril")
 const archive = require('./archiveResolver.js');
 const $ = require("jquery");
 
-let $threadBody = null;
+let $pageBody = null;
 let threadID = 0;
 let pageNum = 1;
 let postID = null;
 let errorMessage = "";
 
+let subforumID = 0;
+
 const ThreadResolver = {
     onmatch: function(args, requestedPath) {
         return new Promise((resolve, reject) => {
-            $threadBody = null;
+            $pageBody = null;
             threadID = 0;
             pageNum = 1;
             postID = null;
@@ -30,7 +32,7 @@ const ThreadResolver = {
                     postID = args.pageNum.match(/\d+$/)[0]
                     pageNum = archive.getPostInfoByID(postID)[1];
                 } else {
-                    pageNum = args.pageNum ? args.pageNum.match(/\d+$/)[0] : 1;
+                    pageNum = args.pageNum.match(/\d+$/)[0];
                 }
             }
 
@@ -42,7 +44,7 @@ const ThreadResolver = {
 
             archive.$getXHRThreadPageByID(threadID, pageNum)
                 .then( $body => {
-                    $threadBody = $body;
+                    $pageBody = $body;
                     resolve();
                 }).catch(err => {
                     errorMessage = err;
@@ -54,27 +56,72 @@ const ThreadResolver = {
             console.log(err);
         });
     },
-    render: function(vnode) {
-        //console.log(vnode);
-        let content = ($threadBody != null) ? m.trust($threadBody.html()) : m('div', `Something went wrong, check the console...\nERROR\n${errorMessage}`);
+    render: function() {
+        let threadBody = ($pageBody != null) ? m.trust($pageBody.html()) : m('div', `Something went wrong, check the console...\nERROR\n${errorMessage}`);
 
-        if ($threadBody != null) {
-            document.title = `FB BMG Broswer - ${$threadBody.find(".p-title-value").text()} - Page ${pageNum}` + (postID == null ? "" : ` Post ${$threadBody.find(`header li a[href$="${postID}"]`).text().trim()}`);
+        if ($pageBody != null) {
+            document.title = `FB BMG Broswer - ${$pageBody.find(".p-title-value").text()} - Page ${pageNum}` + (postID == null ? "" : ` Post ${$pageBody.find(`header li a[href$="${postID}"]`).text().trim()}`);
         } else {
             document.title = 'FB BMG Broswer - ERROR'
         }
 
         return (postID == null) ? 
-            m('div.archivedContent', content) :
+            m('div.archivedContent', threadBody) :
             m('div.archivedContent', {
                 oncreate: () => document.getElementById(`post-${postID}`).scrollIntoView(true),
                 onupdate: () => document.getElementById(`post-${postID}`).scrollIntoView(true) 
-            }, content);
+            }, threadBody);
     }
 }
 
-m.route(document.getElementById("dynamicContent"), "/threads/281788/1",
+const ForumResolver = {
+    onmatch: function(args, requestedPath) {
+        return new Promise((resolve, reject) => {
+            $pageBody = null;
+            subforumID = 0;
+            pageNum = 1;
+
+            subforumID = args.subforumID.match(/\d+$/)[0];
+            
+            if (args.pageNum) {
+                pageNum = args.pageNum.match(/\d+$/)[0];
+            }
+
+            console.log(`Loading Path ${requestedPath}\nLoading subforum/page: ${subforumID}/${pageNum}`);
+
+            archive.$getXHRSubforumByID(subforumID, pageNum)
+                .then( $body => {
+                    $body.find('div.node--unread').removeClass('node--unread');
+                    $body.find('span.node-icon').remove();
+                    $pageBody = $body;
+                    resolve();
+                }).catch(err => {
+                    errorMessage = err;
+                    console.log(err);
+                }
+            )
+        }).catch(err => {
+            errorMessage = err;
+            console.log(err);
+        });
+    },
+    render: function() {
+        let content = ($pageBody != null) ? m.trust($pageBody.html()) : m('div', `Something went wrong, check the console...\nERROR\n${errorMessage}`);
+
+        if ($pageBody != null) {
+            document.title = `FB BMG Broswer - ${$pageBody.find(".p-title-value").text()} - Page ${pageNum}`;
+        } else {
+            document.title = 'FB BMG Broswer - ERROR'
+        }
+
+        return m('div.archivedContent', content);
+    }
+}
+
+m.route(document.getElementById("dynamicContent"), "/forums/fizzy-bubbles.252",
     {
+        "/forums/:subforumID": ForumResolver,
+        "/forums/:subforumID/:pageNum": ForumResolver,
         "/post/:postID": ThreadResolver,
         "/threads/:threadID": ThreadResolver,
         "/threads/:threadID/:pageNum": ThreadResolver
